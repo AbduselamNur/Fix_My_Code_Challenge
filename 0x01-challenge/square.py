48f11fd60b3da0b7fc1fd4cac6a58517dac43ed0
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2023-01-22 21:13:21 +0300

    Fix bug~

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-""" Module For Square Class """
+""" Module for square class"""
 
 
-class square():
-    """
-    square class that has Area and Premiter
-    """
+class Square():
+    """ Square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """Constructor function"""
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -18,18 +16,18 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """Return The Perimeter of The Squre"""
+    def permiter_of_my_square(self):
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Return The width and height"""
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ Create Square Obj"""
-    s = square(width=12, height=9)
+    """ Create a square object """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
